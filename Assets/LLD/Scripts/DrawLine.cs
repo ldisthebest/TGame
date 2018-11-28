5a5915b16d64c39b846273fa13a9b4f55822b67f
Phantomhive123 <949805411@qq.com>
2018-11-28 21:12:36 +0800

    增加游戏界面的格子显示

---
+++ Assets/LLD/Scripts/DrawLine.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrawLine : MonoBehaviour {
+
+    public Material mat;
+
+	// Use this for initialization
+	void Awake () {
+
+        Debug.Log("begin");
+
+        GL.PushMatrix();
+        mat.SetPass(0);
+        GL.LoadPixelMatrix();//设置pixelMatrix 
+        GL.Color(Color.yellow);
+        GL.Begin(GL.LINES);
+
+        for(int i=-150;i<=50;i++)
+        {
+            GL.Vertex3(i, 5f, 0);
+            GL.Vertex3(i, -5f, 0);
+        }
+
+        GL.End();
+        GL.PopMatrix();
+
+        Debug.Log("end");
+	}
+
+}
