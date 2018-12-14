cefbff738ee9e9ed7a883d28e6e8c3f8e979964b
Phantomhive123 <949805411@qq.com>
2018-12-14 14:27:26 +0800

    增加特效和音效

---
+++ Assets/LLD/Scripts/TitleParticleControl.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TitleParticleControl : MonoBehaviour {
+
+    [SerializeField]
+    ParticleSystem particleSystem;
+    [SerializeField]
+    GameObject title;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+        if(Input.GetMouseButtonDown(0))
+        {
+            Action();
+        }
+	}
+
+
+    private void Action()
+    {
+        particleSystem.Play();
+        Destroy(title, 0.65f);
+    }
+}
